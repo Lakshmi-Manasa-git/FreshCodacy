5e83920f98ee72c3dfda1ee2c737a09811b92088
Lakshmi-Manasa-git <85109725+Lakshmi-Manasa-git@users.noreply.github.com>
2021-05-31 16:55:15 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int total = 10 / sum;
+            int[] n = new int[3];
+            for( int i= 0; i <= 3; i++)
+            {
+                n[i]= i;
+            }
+        }
+    }
+}
